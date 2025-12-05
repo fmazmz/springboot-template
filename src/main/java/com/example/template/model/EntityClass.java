b53ca6133651787fa9f58fa335edebacdae63f10
WHITEROSE <firasmoussa60@gmail.com>
2025-12-05 23:15:28 +0100

    create default domain model

---
+++ src/main/java/com/example/template/model/EntityClass.java
@@ -0,0 +1,36 @@
+package com.example.template.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "entity_table")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class EntityClass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column
+    private String name;
+    @Column
+    private String description;
+    @Column
+    private Date createdAt;
+
+}
